51593728360da0cc9466f20459e0fe5b5cfb0122
Sagar <153504870+SagarKharat94440@users.noreply.github.com>
2025-09-03 00:08:22 +0530

    Update api.ts

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://note-taking-six-blue.vercel.app', 
+  baseURL: 'https://note-taking-app-h8ko.vercel.app', 
 });
 
 API.interceptors.request.use(

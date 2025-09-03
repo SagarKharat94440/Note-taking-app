6c672f4879ea90c811255a32b14607686b3f7ad2
Sagar <153504870+SagarKharat94440@users.noreply.github.com>
2025-09-03 16:32:57 +0530

    Add backend URL

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://note-taking-app-h8ko.vercel.app', 
+ baseURL: 'https://note-taking-app-88gl.onrender.com/api'
 });
 
 API.interceptors.request.use(
